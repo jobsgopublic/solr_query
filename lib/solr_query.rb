911f78dee6fee8fa123036a18de064f405c61ca1
Tom Stuart <tom@therye.org>
2011-05-18 14:09:31 +0100

    Our downstream changes (no testing yet, bad boys)

--- lib/solr_query.rb
+++ lib/solr_query.rb
@@ -1,3 +1,7 @@
+unless nil.respond_to?(:blank?)
+  require File.join(File.dirname(__FILE__), "blank")
+end
+
 module SolrQuery
   class << self
     # build a query for solr
@@ -10,53 +14,76 @@ module SolrQuery
     #
     #   SolrQuery.build(:colour => ["red", "pink"], :item_type => ["Toy", "Train"])
     #   => "colour:(red OR pink) AND item_type:(Toy OR Train)"
-    # 
+    #
     # or you can specify a different magical key for keyword;
     #
-    #   SolrQuery.build({:keyword => "old one", :new_keyword => "new one"}, :new_keyword)
+    #   SolrQuery.build({:keyword => "old one", :new_keyword => "new one"}, {:keyword_key => :new_keyword})
     #   => "new one AND keyword:(old one)"
     # if you need to do range queries;
-    # 
+    #
     #   SolrQuery.build(:salary => {:min => "010000", :max => "050000"})
-    #   => "salary:([010000 TO 050000])"
-    # 
+    #   => "salary:(010000 TO 050000)"
+    #
     #   SolrQuery.build(:salary => "010000".."050000")
-    #   => "salary:([010000 TO 050000])"
-    # 
+    #   => "salary:(010000 TO 050000)"
+    #
     #   SolrQuery.build(:surname => {:min => "jacobs")
-    #   => "surname:([jacobs TO *])"
-    def build(conditions = {}, keyword_key=:keyword)
+    #   => "surname:(jacobs TO *)"
+    def build(conditions = {}, opts={})
       conditions = conditions.dup # let's not accidentally kill our original params
+      opts = opts.dup
+      opts[:keyword_key] ||= :keyword
+      opts[:keyword_boost] ||= nil # field name in which keyword relvance should be boosted (via a disgusting hack)
+      opts[:keyword_proximity] ||= 1000 # term proximity required to boost scores based on proximity, see http://wiki.apache.org/solr/SolrRelevancyCookbook
       query_parts = []
-      keyword = conditions.delete(keyword_key) # keyword is magical
-      if !blank?(keyword) # ie. !keyword.blank?
-        query_parts << "#{solr_value(keyword, :downcase => true)}"
+      keyword = conditions.delete(opts[:keyword_key]) # keyword is magical
+      keyword = solr_value(keyword, true, false)
+      unless keyword.blank?
+        if keyword.include?(' OR ') || keyword.include?(' AND ')
+          # backwards compatibility - don't mess with keywords that already contain boolean operators (which effectively means don't mess with keywords provided as an array)
+          query_parts << "#{keyword}"
+        else
+          if keyword.include?(' ')
+            # Find multiple keywords near each other, but also allow for keywords/phrase that ends with "in <some location>".
+            # If keyword provided contains " in ", words before the in are considered to be the keywords that need to
+            # be near each other in the text, words after the in are considered location(s) that can appear anywhere.
+            phrases = keyword.split(' in ') # split keywords in general keywords and location keywords
+            proximity = opts[:keyword_proximity].to_i / phrases.size # if we have both general and location keywords, each set should be nearer each other
+            query_parts << "text:\"#{phrases.shift}\"~#{proximity}" # general keywords
+            query_parts << "text:\"#{phrases.join(' ')}\"~#{proximity}" unless phrases.empty? # other (i.e. location) keywords
+          else
+            query_parts << "#{keyword}"
+          end
+          if opts[:keyword_boost]
+            # Index time boosting not working, so boost score for matches in boost field by explicitly looking for each keyword in that field
+            query_parts[0] = "(" + query_parts[0] + " OR (" + keyword.split(/\s+/).map{|k| "#{opts[:keyword_boost]}:#{k}"}.join(' AND ') + "))"
+          end
+        end
       end
-    
+
       conditions.each do |field, value|
         unless value.nil?
           query_parts << "#{field}:(#{solr_value(value)})"
         end
       end
-      
+
       if query_parts.empty?
         return ""
       else
         return query_parts.join(" AND ")
       end
     end
-  
-    def solr_value(object, opts={})
-      downcase    = opts[:downcase]
-      dont_escape = opts[:dont_escape]
-      
+    
+    protected
+
+    def solr_value(object, downcase=false, clean=false)
       if object.is_a?(Array) # case when Array will break for has_manys
         if object.empty?
           string = "NIL" # an empty array should be equivalent to "don't match anything"
         else
           string = object.map do |element|
-            solr_value(element, opts.merge(:dont_escape => true))
-          end.join(" OR ")
+            solr_value(element, downcase, clean)
+          end.delete_if{|element| element.blank?}.join(" OR ")
           downcase = false # don't downcase the ORs
         end
       elsif object.is_a?(Hash) || object.is_a?(Range)
@@ -64,21 +91,25 @@ module SolrQuery
       elsif defined?(ActiveRecord) && object.is_a?(ActiveRecord::Base)
         string = object.id.to_s
       elsif object.is_a?(String)
-        if downcase && (bits = object.split(" OR ")) && bits.length > 1
-          return "(#{solr_value(bits, opts)})"
+        if downcase && object =~ /\s(OR|AND)\s/
+          string = solr_value(object.gsub(/\s(OR|AND)\s/,'__\1__'), true, clean)
+          string.gsub!('__or__',' OR ')
+          string.gsub!('__and__',' AND ')
+          if !clean && string.include?('(') && string.include?(')') && ( string.scan('(').size == string.scan(')').size )
+            # equal number of opening and closing brackets, un-escape them (yeah, it's not perfect, but it'll do)
+            string.gsub!(/\\+(\(|\))/,'\1')
+          end
+          return '(' + string + ')'
         else
           string = object
         end
       else
         string = object.to_s
       end
-      string.downcase!                    if downcase 
-      string = escape_solr_string(string) unless dont_escape
-      
-      string
+      string.downcase! if downcase
+      return clean ? clean_solr_string(string) : escape_solr_string(string)
     end
-    protected :solr_value
-    
+
     def solr_range(object)
       min = max = nil
       if object.is_a?(Hash)
@@ -90,30 +121,33 @@ module SolrQuery
       end
       min = solr_value(min) if min
       max = solr_value(max) if max
-      
+
       min ||= "*"
       max ||= "*"
-      
+
       return "[#{min} TO #{max}]"
     end
-    protected :solr_range
 
-    def escape_solr_string(string)
-      string.gsub(SOLR_ESCAPE_REGEXP, "\\\\\\0").strip
-    end
-    protected :escape_solr_string
+    def clean_solr_string(str)
+      str.gsub(RE_ESCAPE_LUCENE,'').gsub(/\s+/,' ').strip
+    end 
     
-    def blank?(object) #:nodoc: quick rehash of rails' object.blank?
-      if object.is_a?(String)
-        object !~ /\S/
-      else
-        object.respond_to?(:empty?) ? object.empty? : !object
-      end
-    end
-    protected :blank?
-        
+    def escape_solr_string(str)
+      str.gsub(RE_ESCAPE_LUCENE) { |m| "\\#{m}" }.gsub(/\s+/,' ').gsub(ENDING_KEYWORDS) { |w| w.downcase }.strip
+    end   
   end
+
+  # The Lucene documentation declares special characters to be:
+  #   + - && || ! ( ) { } [ ] ^ " ~ * ? : \
+  # and I've added a semi-colon, because I find them offensive ;-)
+  # note: this nice code comes from Jeremy Voorhis's Lucene query builder at http://github.com/jvoorhis/lucene_query
+  RE_ESCAPE_LUCENE = /
+    ( [-+!\(\)\{\}\[\]^"~*?:;\\] # A special character
+    | &&                         # Boolean &&
+    | \|\|                       # Boolean ||
+    )
+  /x 
   
-  SOLR_ESCAPE_CHARACTERS = %w" \\ + - ! ( ) : ; ^ [ ] { } ~ * ? "
-  SOLR_ESCAPE_REGEXP = Regexp.new(SOLR_ESCAPE_CHARACTERS.map{|char| Regexp.escape(char)}.join("|"))
+  ENDING_KEYWORDS = /(AND$ | OR$ | NOT$)/x
+
 end
--- spec/solr_query_spec.rb
+++ spec/solr_query_spec.rb
@@ -13,39 +13,36 @@ describe SolrQuery do
     before(:each) do
       @record = Organisation.new(45)
       @it = SolrQuery
-      
-      @backslash = '\\'
-      @escaped_backslash = @backslash*2 # backslashes are difficult to work with
     end
-    
+
     it "should leave multi-word keywords intact" do
       @it.build(:keyword => "abc def").should == "abc def"
     end
-    
+
     it "should default to giving out an empty string" do
       @it.build().should == ""
     end
-    
+
     it "should join an array keyword with ORs" do
       @it.build(:keyword => ["abc", 23, @record]) \
         .should == "abc OR 23 OR 45"
     end
-    
+
     it "should lowercase keywords" do
       @it.build(:keyword => "SOMETHING mostly UPPER CAse") \
         .should == "something mostly upper case"
     end
-    
+
     it "should lowercase an array of keywords, but not the ORs" do
       @it.build(:keyword => ["SOMETHING", "mostly", "UPPER", "CAse"]) \
         .should == "something OR mostly OR upper OR case"
     end
-    
+
     it "should join an array field with ORs" do
       @it.build(:someth => ["abc", 23, @record]) \
         .should == "someth:(abc OR 23 OR 45)"
     end
-    
+
     it "should add a 'NIL' if given an empty array as a condition" do
       @it.build(:empty => []) \
         .should == "empty:(NIL)"
@@ -55,22 +52,22 @@ describe SolrQuery do
       @it.build(:keyword => []) \
         .should == ""
     end
-    
+
     it "should ignore the keyword field if given as nil" do
       @it.build(:keyword => nil, :something => "else") \
         .should == "something:(else)"
     end
-    
+
     it "should ignore the keyword field if given as empty" do
       @it.build(:keyword => "   ", :something => "else") \
         .should == "something:(else)"
     end
-    
+
     it "should let us define a string with an OR statement in" do
       @it.build(:keyword => "fire OR theft", :something => "else") \
         .should == "(fire OR theft) AND something:(else)"
     end
-    
+
     it "should make a Record into its id" do
       @it.build(:keyword => @record, :record => @record).should == "45 AND record:(45)"
     end
@@ -78,58 +75,48 @@ describe SolrQuery do
     it "should ignore a nil field" do
       @it.build(:nilly => nil, :truey => true).should == "truey:(true)"
     end
-    
+
     it "should default if just given nils" do
       @it.build(:nilly => nil, :nully => nil, :keyword => nil).should == ""
     end
-    
-    it 'should escape any \ ' do
-      @it.build(:keyword => @backslash+" yeah"+@backslash+' '+@backslash+'    ', :something => 'a '+@backslash+'ey'+@backslash) \
-        .should == @escaped_backslash+' yeah'+@escaped_backslash+' '+@escaped_backslash+' AND something:(a '+@escaped_backslash+'ey'+@escaped_backslash+')'
-    end
-    
+
     # this is a magical set of specs, that may be fragile - so we'll do a less magic version in a mo
-    [ '+', '-', '!', '(', ')', ':', ';', '^', '[', ']', '{', '}', '~', '*', '?' ].each do |char|
+    SolrQuery::SOLR_ESCAPE_CHARACTERS.each do |char|
       it "should escape any #{char}" do
         @it.build(:keyword => "#{char} yeah#{char} #{char}   ", :something => "a #{char}ey#{char}").should == "\\#{char} yeah\\#{char} \\#{char} AND something:(a \\#{char}ey\\#{char})"
       end
     end
-    
-    it 'should escape values in an array correctly' do
-      @it.build(:id => ['User:1', 'User:2']) \
-        .should == 'id:(User\:1 OR User\:2)'
-    end
-    
+
     it "should avoid matching anything if given an empty array as a value" do
       @it.build(
         :keyword => ["dont match", "something", "real"], :if_weve_got_a => []
         ).should == 'dont match OR something OR real AND if_weve_got_a:(NIL)'
     end
-    
+
     it "should escape lucene special characters" do
       @it.build(:keyword => '  a* in the(sky)?  ', :something => 'makes {little} ~ferrets die! ').should == 'a\* in the\(sky\)\? AND something:(makes \{little\} \~ferrets die\!)'
     end
-    
+
     it "should convert a range to a solr_range" do
       @it.build(:range => "abc".."def"
       ).should == "range:([abc TO def])"
     end
-    
+
     it "should convert a range hash to a solr_range" do
       @it.build(:range => {:min => "abc", :max => "def"}
       ).should == "range:([abc TO def])"
     end
-    
+
     it "should convert a >= range hash to a solr_range" do
       @it.build(:range => {:min => "abc"}
       ).should == "range:([abc TO *])"
     end
-    
+
     it "should convert a <= range hash to a solr_range" do
       @it.build(:range => {:max => "def"}
       ).should == "range:([* TO def])"
     end
-    
+
     it "should convert an empty hash to a wild solr_range" do
       @it.build(:range => {}
       ).should == "range:([* TO *])"
